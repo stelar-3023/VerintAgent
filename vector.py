c9b722812f429d3d24b5ff932c87a40e67c683a8
Steve Larsen <stelar1@five9.com>
2025-07-21 13:14:05 -0500

    removed lockfile logic

--- vector.py
+++ vector.py
@@ -5,8 +5,6 @@ def get_retriever():
     from langchain_openai import OpenAIEmbeddings
     from langchain_chroma import Chroma
 
-
-
     pdf_dir = "./pdfs"
     pdf_paths = [os.path.join(pdf_dir, f)
                  for f in os.listdir(pdf_dir) if f.endswith(".pdf")]
@@ -26,17 +24,11 @@ def get_retriever():
 
     embedding = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
 
-    db_location = "./chroma_langchain_db"
     vectorstore = Chroma(
         collection_name="pdf_docs",
         embedding_function=embedding
     )
 
-    # if not os.path.exists(db_location + "/index.lock"):
-    #     print("Indexing new documents...")
-    #     vectorstore.add_documents(split_docs)
-    #     open(db_location + "/index.lock", "w").close()
-    # else:
-    #     print("Using existing Chroma DB")
+    vectorstore.add_documents(split_docs)
 
     return vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 10})
